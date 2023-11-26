4702ee36806e6a36f013c304f6238e72a94ea8c3
Rusty Shackleford <rusty@shackleford.me>
2023-11-26 13:07:30 -0600

    Update edge user agent to Chrome/116.0.0.0

--- src/constants.js
+++ src/constants.js
@@ -50,5 +50,5 @@ const constants = Object.freeze({
   MOBILE_USER_AGENTS: [
     'Mozilla/5.0 (Linux; Android 10; SM-G973F Build/QP1A.190711.020; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36',
   ],
-  EDGE_USER_AGENT: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36 Edg/88.0.705.63',
+  EDGE_USER_AGENT: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.81',
 });
